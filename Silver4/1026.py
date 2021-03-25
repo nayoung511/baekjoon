c944be4ce970168efad78f95f22f359630363083
nayoung <nayoungbae511@gmail.com>
2021-03-25 23:46:03 +0900

    3/25 -

---
+++ Silver4/1026.py
@@ -0,0 +1,14 @@
+def main():
+    # A의 수를 재배열 
+    # B의 수는 재배열 x 
+    c = int(input())
+
+    #가장 큰 수와 가장 작은 수 매칭
+    for i in range (c):
+        a = list(map(int, input().split()))
+        b = list(map(int, input().split()))
+
+
+
+
+main()
