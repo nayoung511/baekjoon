8b71d93a82c0df2b0b84c1c445e28044be0b5666
nayoung <nayoungbae511@gmail.com>
2021-03-26 01:12:22 +0900

    3/26

--- Silver4/1026.py
+++ Silver4/1026.py
@@ -1,14 +1,19 @@
 def main():
     # A의 수를 재배열 
     # B의 수는 재배열 x 
-    c = int(input())
+    i = int(input())
 
     #가장 큰 수와 가장 작은 수 매칭
-    for i in range (c):
-        a = list(map(int, input().split()))
-        b = list(map(int, input().split()))
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
 
+    a.sort()
+    e = b.copy()
+    e.sort(reverse=True)
+    sum = 0
 
-
+    for j in range(i):
+        sum = sum + (e[j] * a[j])
+    print(sum)
 
 main()
