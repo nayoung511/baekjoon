05c88c386cdebe9d564f18e75f6a459d7ad4e108
nayoung <nayoungbae511@gmail.com>
2021-03-28 22:58:31 +0900

    3/28

--- Silver4/2108.py
+++ Silver4/2108.py
@@ -1,27 +1,31 @@
 from collections import Counter
+from sys import stdin
 
 def main():
     n = int(input())
     a = []
-    sum = 0
 
     for i in range(n):
-        c = int(input())
-        sum = sum + c
-        a.append(c)
+        c = stdin.readline()
+        a.append(int(c))
 
+    print(a)
     a.sort()
     #1. 산술평균
-    print(sum // n)
+    print(round(sum(a) / n))
 
     #2. 중앙값
-    mid = n//2
-    print(a[mid])
+    if n % 2 == 1:
+        mid = n//2
+        print(a[mid])
+    else:
+        print((a[n//2 + 1] + a[n//2]) // 2)
     
     #3. 최빈값
     b = Counter(a).most_common()
     if len(b) > 1:
-        print(b[1][0])
+        if b[0][1] == b[1][1]:
+            print(b[1][0])
     else:
         print(b[0][0])
 
