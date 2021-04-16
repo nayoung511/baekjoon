59f7d67458f7361d5f40c3248a9620cdd6ec685f
nayoung <nayoungbae511@gmail.com>
2021-04-16 23:51:28 +0900

    4/16

---
+++ Silver4/2659.py
@@ -0,0 +1,16 @@
+from sys import stdin
+
+def sipja(num):
+    a = [num]
+    for i in range (1, 4):
+        print(num[1:], num[0:1])
+        num = num[1:] + num[0:1]
+        a.append(num)
+    return min(a)
+        
+n = input().split(" ")
+
+n = sipja(n)
+count = 0
+for i in range (1111, n):
+    if sipja(i) == n:
