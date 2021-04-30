c86ba1e053700abe7c8f71dcd10d1f2b2bec9644
배나영 <nayoungbae511@gmail.com>
2021-04-30 22:22:02 +0900

    4/30

--- Silver4/13699.py
+++ Silver4/13699.py
@@ -1,13 +1,15 @@
 n = int(input())
-x = [0] *(10**6)
+x = [0] *(n+1)
+x[0] = 1
 
 def dp(n):
     if n == 0: return 1
+    if n == 1: return 1
     if x[n] != 0: return x[n]
     else:
         val = 0
         for i in range (n):
-            val += dp(i) * dp(n-i)
+            val += dp(i) * dp(n-i-1)
         x[n] = val
     return x[n]
 
