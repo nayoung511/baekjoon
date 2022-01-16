cf21ad01c3318f0a29e9f57b8c410ee135de2656
배나영 <nayoungbae511@gmail.com>
2022-01-16 20:30:00 +0800

    1/16

--- not solved/1874.py
+++ not solved/1874.py
@@ -1,50 +1,70 @@
 from collections import deque
 from sys import stdin
 n = int(input())
+
+# num 넣을 리스트
 a = []
+
+# 정답 넣을 리스트
+ans = []
+
+#queue
 q = deque()
-sign = []
-val = []
+
+# pop 완료된 리스트 // a 와 같아야 함
+check = []
 
 for i in range (n):
     inp = int(stdin.readline())
     a.append(inp)
 
-start = 1
-idx = 0
-while len(sign) != len(a):
-    if start < a[idx]:
-        #차이만큼 push
-        for _ in range (a[idx] - start+1):
-            q.append(start)
-            val.append("+")
-            
-            if start == a[idx]:
-                sign.append(q.pop())
-                val.append("-")
-                
-            start = start + 1
 
-    if start == a[idx] or len(q) == 0:
-        q.append(start)
-        val.append("+")
-        start = start + 1
+start = 0
+val = 1
+flag = True
+while len(check) != len(a):
 
-        sign.append(q.pop())
-        val.append("-")
+# if the last element in the list equls to the element in a
+    if len(q) != 0:
+        
+        # 큐의 첫 원소를 확인하고 다시 넣어줌
+        first = q.pop()
+        q.append(first)
 
-    else:
-        for _ in range (start - a[idx]-1):
-            sign.append(q.pop())
-            val.append("-")
+        if first == a[start]: 
+            #print(start, first, a[start])
+            #print("-")
+            ans.append("-")
+            #print("gonna append", first)
+            check.append(first)
+            q.pop()
+            #print(q, "check:   ", check)
+            start += 1
 
-            if len(q) == 0:
+        else:
+            if val >= a[start] + 1:
+                flag = False
+                #print("NO")
                 break
-    idx = idx + 1
-
-if sign != a:
-    print("NO")
-else:
-    for i in val:
-        print(i)
+            for i in range (val, a[start]+1):
+                #print(val, a[start], i, start)
+                q.append(i)
+                #print("+", q)
+                ans.append("+")
+                val = a[start] +1
+            
+        
+    # 빈 큐일 때 필요한 횟수만큼 집어 넣는다
+    if len(q) == 0 and len(check) != len(a):
+        for i in range (val, a[start]+1):
+            #print(val, a[start], i, start)
+            q.append(i)
+            #print("+", q)
+            ans.append("+")
+        val = a[start] + 1
+ 
     
+if flag == True:
+    print(*ans, sep="\n")
+else:
+    print("NO")
