c0fc04530f710ebe882e49c23a633677ac22b4cc
배나영 <nayoungbae511@gmail.com>
2022-01-23 03:21:00 +0800

    1/22

--- Silver3/2003.py
+++ Silver3/2003.py
@@ -1,29 +1,39 @@
 n, m = map(int, input().split())
 
 num = list(map(int, input().split()))
+
 start, end = 0, 1
 sum = num[start]
 count = 0
-while (start < end and end <= n-1):
-    if sum == m:
-        count+= 1
-        start += 1
-        end = start + 1
+
+if len(num) == 1:
+    if num[0] == m:
+        count = 1
     else:
-        print(start, end, count)
-        if sum + num[end] < m:
-            end += 1
-            sum += num[end]
+        count = 0
 
-        elif sum + num[end] > m:
-            start+=1
-            end = start + 1
-
-        elif sum + num[end] == m:
+else:
+    while (end < n):
+        if sum + num[end] == m or sum == m:
             count += 1
             start += 1
             end = start + 1
-    sum = num[start]
-    #print(start, end)
+            sum = num[start]
+        # sum < m
+        elif sum + num[end] < m:
+            # end += 1
+            sum += num[end]
+            end += 1
+        # num[start] + num[end] >= m
+        elif sum + num[end] > m or num[start] == m:
+            if end == n-1 and num[end] == m:
+                count += 1
+                break
+            # start += 1
+            # end = start + 1
+            else:
+                start += 1
+                end = start + 1
+                sum = num[start]
 
 print(count)
