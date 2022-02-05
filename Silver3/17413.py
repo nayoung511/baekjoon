e081cd1e461def03aa2685e77b9ed87a0acb0c9d
배나영 <nayoungbae511@gmail.com>
2022-02-06 00:08:27 +0800

    2/5

--- Silver3/17413.py
+++ Silver3/17413.py
@@ -2,9 +2,63 @@ from sys import stdin
 from collections import deque
 
 d = deque()
+rev = deque()
+
 s = list(stdin.readline().rstrip())
 
 for i in range (len(s)):
     d.append(s[i])
+count = 0
 
-print(d)
+while len(d) > 0:
+    # tag 인지 확인 
+    a = d.popleft()
+    d.appendleft(a)
+
+    if a == '<':
+        b = d.popleft()
+        d.appendleft(b)
+
+        while b != '>':
+            print(b, end='')
+            d.popleft()
+            #print("d", d)
+            b = d.popleft()
+            d.appendleft(b)
+        
+        b = d.popleft()
+        d.appendleft(b)
+
+        print(b, end='')
+        d.popleft()
+
+    elif a == ' ':
+        print(a, end='')
+        d.popleft()
+
+    else:
+        b = d.popleft()
+        d.appendleft(b)
+
+        # space가 아닐 때까지 
+        while b != ' ' and  b != '<' and len(d) > 0:
+            rev.appendleft(b)
+            d.popleft()
+            #print("d", d)
+            if len(d) > 0:
+                b = d.popleft()
+                d.appendleft(b)
+                #print(d)
+
+        while len(rev) != 0:
+            b = rev.popleft()
+            rev.append(b)
+
+            print(b, end='')
+
+            rev.pop()
+
+#print(d, rev)
+
+
+
