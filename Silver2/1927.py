2a8dba4b7c8cce4361451eac6abaa0b11d6e6649
NAYOUNG <nayoungbae511@gmail.com>
2022-02-19 16:59:32 +0800

    2/19

---
+++ Silver2/1927.py
@@ -0,0 +1,21 @@
+from queue import PriorityQueue
+import sys
+
+input=sys.stdin.readline
+n = int(input())
+num = PriorityQueue()
+for i in range (n):
+    inp = int(input())
+
+    if inp == 0:
+        if num.empty():
+            print(0)
+        else:
+            print(num.get())
+            #num.pop
+    else:
+        num.put(inp)
+
+#num_set = set(num)
+
+#print(num_set)
