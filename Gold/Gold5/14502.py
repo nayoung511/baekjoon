3eac8e05d022db2e2764750e3457b3bce9b3670e
NAYOUNG BAE <nayoungbae511@gmail.com>
2022-03-20 22:17:30 +0800

    3/20

--- Gold/Gold5/14502.py
+++ Gold/Gold5/14502.py
@@ -1,83 +1,48 @@
 import sys
-from collections import deque
+import copy
+
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for i in range (n)]
-minn = 0
 
-def count():
-    count = 0
-    for i in range (n):
-        for j in range (m):
-            if graph[i][j] == 0:
-                count += 1
-
-    return count
-
-# calculate first count
-maxx = count()
-print(maxx)
-q = deque([])
+maxx = 0
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
-
-def wall(x, y):
-    global minn
-    wall = 3
-    q.append([x, y])
-
-    while q:
-        ex, ey = q.popleft()
-
-        for i in range (4):
-            nx = ex + dx[i]
-            ny = ey + dy[i]
-
-            if 0<nx<n and 0<ny<m:
-                if graph[nx][ny] == 0:
-                    graph[nx][ny] = 1
-                    wall -= 1
-                    q.append([nx, ny])
-
-        
-                if wall == 0:
-                    minn = count()
-
-                    print(minn)
-                    if maxx > minn:
-                        return minn
-
-
+virus_list = []
 for i in range (n):
     for j in range (m):
         if graph[i][j] == 2:
-            wall(i, j)
+            virus_list.append([i, j])
 
+def wall(start, count):
+    global maxx
+    if count == 3:
+        selNM = copy.deepcopy(graph)
+        for i in range (len(virus_list)):
+            x, y = virus_list[i]
+            spread_virus(x, y, selNM)
+        c = sum(j.count(0) for j in selNM)
+        maxx= max(maxx, c)
+    else:
+        for i in range (start, n*m):
+            x = i // m
+            y = i % m
+            if graph[x][y] == 0:
+                graph[x][y] = 1
+                wall(i, count+1)
+                graph[x][y] = 0
 
-print(graph)
-
-def virus(x, y):
-    q.append([x, y])
-
-    while q:
-        ex, ey = q.popleft()
-
+def spread_virus(x, y, selNM):
+    if selNM[x][y] == 2:
         for i in range (4):
-            nx = ex + dx[i]
-            ny = ey + dy[i]
-
-            if 0<nx<n and 0<ny<m:
-                if graph[nx][ny] == 0:
-                    graph[nx][ny] = 2
-                    q.append([nx, ny])
-
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0<=nx<n and 0<=ny<m:
+                if selNM[nx][ny] == 0:
+                    selNM[nx][ny] = 2
+                    spread_virus(nx, ny, selNM)
 
 
-for i in range (n):
-    for j in range (m):
-        if graph[i][j] == 2:
-            virus(i, j)
-
-print(count())
-print(minn)
+wall(0,0)
+print(maxx)
