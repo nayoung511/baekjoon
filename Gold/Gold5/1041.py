eb1b49c899be58e55c1e02ce5aa64a1dbb9db961
배나영 <nayoungbae511@gmail.com>
2022-05-31 21:20:26 +0800

    5/31

---
+++ Gold/Gold5/1041.py
@@ -0,0 +1,48 @@
+n = int(input())
+# A, B, C, D, E, F
+num = list(map(int, input().split()))
+
+if n == 1:
+    print(sum(num) - max(num))
+else:
+    # 면 1, 2, 3개 
+    side1, side2, side3 = 0, 0, 0
+
+    side1 = min(num)
+
+    # 만날 수 없는 짝
+    # A - F
+    # D - C
+    # E - B
+
+    side2 = min(
+        num[0]+num[1],
+        num[0]+num[2],
+        num[0]+num[3],
+        num[0]+num[4],
+        num[1]+num[2],
+        num[1]+num[3],
+        num[1]+num[5],
+        num[2]+num[4],
+        num[2]+num[5],
+        num[3]+num[4],
+        num[3]+num[5],
+        num[4]+num[5]
+    )
+
+    side3 = min(
+        num[0]+num[1]+num[2],
+        num[0]+num[1]+num[3],
+        num[0]+num[3]+num[4],
+        num[0]+num[2]+num[4],
+        num[1]+num[2]+num[5],
+        num[1]+num[3]+num[5], 
+        num[2]+num[4]+num[5], 
+        num[3]+num[4]+num[5]
+    )
+
+    summ = side1 * ((n-2) * (n-2) + (n-1)*(n-2) * 4)
+    summ += side2 * ((n-2) * 4 + (n-1) * 4)
+    summ += side3 * 4
+
+    print(summ)
