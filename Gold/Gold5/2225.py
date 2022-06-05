18e73678b4f039d69f1c976df6fd885434b432ca
배나영 <nayoungbae511@gmail.com>
2022-06-06 01:13:34 +0800

    6/6

--- Gold/Gold5/2225.py
+++ Gold/Gold5/2225.py
@@ -1,13 +1,13 @@
 n, k = map(int, input().split())
-dp=[[0 for i in range (n+1)] for j in range (k+1)]
+dp=[[0 for i in range (201)] for j in range (201)]
 
-for i in range (n+1):
+for i in range (201):
     dp[1][i] = 1
     dp[2][i] = i+1
 
-for i in range (2, k+1):
+for i in range (2, 201):
     dp[i][1]=i
-    for j in range (2, n+1):
+    for j in range (2, 201):
         dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000000
 
 print(dp[k][n])
