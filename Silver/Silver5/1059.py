5a81d44c037a43a3e1e432d648d292a12bfec8ab
nayoung511 <nayoungbae511@gmail.com>
2022-07-10 02:55:51 +0800

    7/10

--- Silver/Silver5/1059.py
+++ Silver/Silver5/1059.py
@@ -3,12 +3,13 @@ input = sys.stdin.readline
 
 l = int(input())
 s = list(map(int, input().split()))
+s.append(0)
 s.sort()
 n = int(input())
 
 count = 0
-for i in range (l-1):
-    if s[i] == n:
+for i in range (l):
+    if s[i] == n or s[i+1] == n:
         break
     elif s[i] < n and s[i+1] > n:
         count = (n - s[i]) * (s[i+1] - n) - 1
